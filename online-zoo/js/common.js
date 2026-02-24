c732ed3a9bbb9caaf1a420231a1aea509273915f
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-02-24 18:03:35 +0400

    feat: init map page and add header and footer to it

--- online-zoo/js/common.js
+++ online-zoo/js/common.js
@@ -4,7 +4,6 @@ const navLinks = document.querySelectorAll(".page-navigation a");
 navLinks.forEach((link) => {
   const href = link.getAttribute("href");
   const formated = href.replace("..", "");
-  console.log(formated);
   if (path.endsWith(formated)) {
     link.classList.add("active");
   }
