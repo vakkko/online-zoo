ee82a12a287d2d75edf009e679375feee5f84df0
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-02-26 20:56:11 +0400

    refactor: javascript logic and change the href from animal pages in order to achieve active state for page links

--- online-zoo/js/common.js
+++ online-zoo/js/common.js
@@ -3,7 +3,8 @@ const navLinks = document.querySelectorAll(".page-navigation a");
 
 navLinks.forEach((link) => {
   const href = link.getAttribute("href");
-  const formated = href.replace("..", "");
+  const formated = href.replaceAll("../", "");
+
   if (path.endsWith(formated)) {
     link.classList.add("active");
   }
